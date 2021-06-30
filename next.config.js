da2186e2b8911d849e0839f2bba5e0bf388ede69
Ehtisham Zaheer <65091011+ehtisham80@users.noreply.github.com>
2021-06-30 20:18:58 +0200

    created booking-hotel-app
    created booking-hotel-app

---
+++ next.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    env: {
+        DB_LOCAL_URI: '',
+        DB_URI: '',
+
+        STRIPE_API_KEY: '',
+        STRIPE_SECRET_KEY: '',
+
+        STRIPE_WEBHOOK_SECRET: '',
+
+        CLOUDINARY_CLOUD_NAME: '',
+        CLOUDINARY_API_KEY: '',
+        CLOUDINARY_API_SECRET: '',
+
+        SMTP_HOST: "",
+        SMTP_PORT: "",
+        SMTP_USER: "",
+        SMTP_PASSWORD: "",
+        SMTP_FROM_EMAIL: "noreply@bookit.com",
+        SMTP_FROM_NAME: "",
+
+        NEXTAUTH_URL: '',
+    },
+    images: {
+        domains: ['res.cloudinary.com'],
+    },
+}
